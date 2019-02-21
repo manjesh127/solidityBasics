6f4ba50d9f190ebcbcf6891fb6cc62df5592df99
Manjesh <majeshsingh127@gmail.com>
2019-02-21 12:24:03 +0530

    how to define arrayAndStructure

---
+++ tute_3_StructureAndArray.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract arrayDemo{
+    
+    struct student{
+        uint id;
+        string name;
+        
+    }
+    uint[] array1;      //dynamic array
+    uint[20] array2;    //static array 
+}
